b379a04a64abdd2cdde4e5a84fa9aa3a3cd6b3dc
Patrick Lemmer <patrick.lemmer@yahoo.com>
2023-02-25 23:45:20 +0100

    set up project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// Imports
+// React imports
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+// Page imports
+import Home from './pages/Home';
+import About from './pages/About';
+import Vans from './pages/Vans';
+
+// Stylesheet imports
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <header>
+        <Link to="/">#Vanlife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        skin: {
+          // Background color
+          'bg-nav': 'var(--color-bg-navbar)',
+          'bg-footer': 'var(--color-bg-footer)',
+          'bg-accent': 'var(--color-bg-accent)',
+          'bg-about-cta': 'var(--color-bg-about-cta)',
+        },
+      },
+    },
+    screens: {
+      xxs: '300px',
+      xs: '480px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+    },
   },
   plugins: [],
 };
