f83189d3746e07db81d0cdc7c069f2a165d31837
Patrick Lemmer <patrick.lemmer@yahoo.com>
2023-03-13 19:25:07 +0100

    added loader, defer, Await and Suspense

--- src/api.js
+++ src/api.js
@@ -12,6 +12,19 @@ export async function getVans() {
   return data.vans;
 }
 
+export async function getHostVans() {
+  const res = await fetch('/api/host/vans');
+  if (!res.ok) {
+    throw {
+      message: 'Failed to fetch vans',
+      statusText: res.statusText,
+      status: res.status,
+    };
+  }
+  const data = await res.json();
+  return data.vans;
+}
+
 export async function loginUser(creds) {
   const res = await fetch('/api/login', {
     method: 'post',
--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,7 @@ import VanDetail from './pages/Vans/VanDetail';
 import Dashboard from './pages/Host/Dashboard';
 import Income from './pages/Host/Income';
 import Reviews from './pages/Host/Reviews';
-import HostVans from './pages/Host/HostVans';
+import HostVans, { loader as hostVansLoader } from './pages/Host/HostVans';
 import HostVanDetail from './pages/Host/HostVanDetail';
 import HostVanInfo from './pages/Host/HostVanInfo';
 import HostVanPhotos from './pages/Host/HostVanPhotos';
@@ -62,7 +62,7 @@ const router = createBrowserRouter(
           <Route index element={<Dashboard />} />
           <Route path="income" element={<Income />} />
           <Route path="reviews" element={<Reviews />} />
-          <Route path="vans" element={<HostVans />} />
+          <Route path="vans" element={<HostVans />} loader={hostVansLoader} />
           <Route path="vans/:id" element={<HostVanDetail />}>
             <Route index element={<HostVanInfo />} />
             <Route path="pricing" element={<HostVanPricing />} />
