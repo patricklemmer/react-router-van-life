bf32f410eb3ad7f5849072f861df122909879ff3
Patrick Lemmer <patrick.lemmer@yahoo.com>
2023-03-13 17:25:43 +0100

    refactored loader func to use deferred data

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,7 +1,13 @@
 // Imports
 // React imports
 import React, { useState } from 'react';
-import { Link, useSearchParams, useLoaderData, defer } from 'react-router-dom';
+import {
+  Link,
+  useSearchParams,
+  useLoaderData,
+  defer,
+  Await,
+} from 'react-router-dom';
 
 // Other imports
 import { getVans } from '../../api';
@@ -13,38 +19,9 @@ export function loader() {
 function Vans() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [error, setError] = React.useState(null);
-  const vans = useLoaderData();
+  const dataPromise = useLoaderData();
 
   const typeFilter = searchParams.get('type');
-
-  const displayedVans = typeFilter
-    ? vans.filter((van) => van.type === typeFilter)
-    : vans;
-
-  const vanElements = displayedVans.map((van) => {
-    return (
-      <div key={van.id} className="van-card grid justify-items-start">
-        <Link
-          to={`${van.id}/${van.name}`}
-          state={{
-            search: `?${searchParams.toString()}`,
-            type: typeFilter,
-          }}
-        >
-          <img src={van.imageUrl} alt="Van image" className="rounded-md mb-2" />
-          <div className="grid justify-items-start gap-2 mb-1">
-            <h3 className="font-bold xxs:text-lg xs:text-xl">{van.name}</h3>
-            <p className="text-[0.9rem] xxs:text-base xs:text-lg">
-              {van.price}€<span>/day</span>
-            </p>
-            <i className={`van-type ${van.type} selected text-sm text-left`}>
-              {van.type}
-            </i>
-          </div>
-        </Link>
-      </div>
-    );
-  });
 
   function handleFilterChange(key, value) {
     setSearchParams((prevParams) => {
@@ -66,41 +43,85 @@ function Vans() {
       <h1 className="text-left font-extrabold text-lg mb-6 xxs:text-xl xs:text-2xl xs:mb-7 sm:text-4xl sm:mb-10">
         Explore our van options
       </h1>
-      <div className="flex flex-wrap gap-2 text-sm justify-center mb-6">
-        <button
-          onClick={() => handleFilterChange('type', 'simple')}
-          className={`van-type simple ${
-            typeFilter === 'simple' ? 'selected' : ''
-          }`}
-        >
-          Simple
-        </button>
-        <button
-          onClick={() => handleFilterChange('type', 'rugged')}
-          className={`van-type rugged ${
-            typeFilter === 'rugged' ? 'selected' : ''
-          }`}
-        >
-          Rugged
-        </button>
-        <button
-          onClick={() => handleFilterChange('type', 'luxury')}
-          className={`van-type luxury ${
-            typeFilter === 'luxury' ? 'selected' : ''
-          }`}
-        >
-          Luxury
-        </button>
-        {/* Renders "Clear filter" btn only of any filter is currently applied */}
-        {typeFilter ? (
-          <button onClick={() => handleFilterChange('type', null)}>
-            Clear filter
-          </button>
-        ) : null}
-      </div>
-      <div className="grid grid-cols-1 gap-6 xs:grid-cols-2 xs:gap-8 md:grid-cols-3 md:gap-10">
-        {vanElements}
-      </div>
+      <Await resolve={dataPromise.vans}>
+        {(vans) => {
+          const displayedVans = typeFilter
+            ? vans.filter((van) => van.type === typeFilter)
+            : vans;
+
+          const vanElements = displayedVans.map((van) => {
+            return (
+              <div key={van.id} className="van-card grid justify-items-start">
+                <Link
+                  to={`${van.id}/${van.name}`}
+                  state={{
+                    search: `?${searchParams.toString()}`,
+                    type: typeFilter,
+                  }}
+                >
+                  <img
+                    src={van.imageUrl}
+                    alt="Van image"
+                    className="rounded-md mb-2"
+                  />
+                  <div className="grid justify-items-start gap-2 mb-1">
+                    <h3 className="font-bold xxs:text-lg xs:text-xl">
+                      {van.name}
+                    </h3>
+                    <p className="text-[0.9rem] xxs:text-base xs:text-lg">
+                      {van.price}€<span>/day</span>
+                    </p>
+                    <i
+                      className={`van-type ${van.type} selected text-sm text-left`}
+                    >
+                      {van.type}
+                    </i>
+                  </div>
+                </Link>
+              </div>
+            );
+          });
+          return (
+            <>
+              <div className="flex flex-wrap gap-2 text-sm justify-center mb-6">
+                <button
+                  onClick={() => handleFilterChange('type', 'simple')}
+                  className={`van-type simple ${
+                    typeFilter === 'simple' ? 'selected' : ''
+                  }`}
+                >
+                  Simple
+                </button>
+                <button
+                  onClick={() => handleFilterChange('type', 'rugged')}
+                  className={`van-type rugged ${
+                    typeFilter === 'rugged' ? 'selected' : ''
+                  }`}
+                >
+                  Rugged
+                </button>
+                <button
+                  onClick={() => handleFilterChange('type', 'luxury')}
+                  className={`van-type luxury ${
+                    typeFilter === 'luxury' ? 'selected' : ''
+                  }`}
+                >
+                  Luxury
+                </button>
+                {/* Renders "Clear filter" btn only of any filter is currently applied */}
+                {typeFilter ? (
+                  <button onClick={() => handleFilterChange('type', null)}>
+                    Clear filter
+                  </button>
+                ) : null}
+              </div>
+              <div className="grid grid-cols-1 gap-6 xs:grid-cols-2 xs:gap-8 md:grid-cols-3 md:gap-10">
+                {vanElements}
+              </div>
+            </>
+          );
+        }}
+      </Await>
     </div>
   );
 }
