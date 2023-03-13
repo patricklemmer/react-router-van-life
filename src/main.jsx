8aedf787b3a2ae44b10b54b410d6e3daa12ab55b
Patrick Lemmer <patrick.lemmer@yahoo.com>
2023-03-13 21:02:23 +0100

    added loader, defer, Await and Suspense

--- src/main.jsx
+++ src/main.jsx
@@ -21,7 +21,9 @@ import Dashboard from './pages/Host/Dashboard';
 import Income from './pages/Host/Income';
 import Reviews from './pages/Host/Reviews';
 import HostVans, { loader as hostVansLoader } from './pages/Host/HostVans';
-import HostVanDetail from './pages/Host/HostVanDetail';
+import HostVanDetail, {
+  loader as hostVansDetailLoader,
+} from './pages/Host/HostVanDetail';
 import HostVanInfo from './pages/Host/HostVanInfo';
 import HostVanPhotos from './pages/Host/HostVanPhotos';
 import HostVanPricing from './pages/Host/HostVanPricing';
@@ -67,7 +69,11 @@ const router = createBrowserRouter(
           <Route path="income" element={<Income />} />
           <Route path="reviews" element={<Reviews />} />
           <Route path="vans" element={<HostVans />} loader={hostVansLoader} />
-          <Route path="vans/:id" element={<HostVanDetail />}>
+          <Route
+            path="vans/:id"
+            element={<HostVanDetail />}
+            loader={hostVansDetailLoader}
+          >
             <Route index element={<HostVanInfo />} />
             <Route path="pricing" element={<HostVanPricing />} />
             <Route path="photos" element={<HostVanPhotos />} />
--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -1,11 +1,24 @@
 // Imports
 // React imports
-import React, { useState, useEffect } from 'react';
-import { useParams, Link, NavLink, Outlet } from 'react-router-dom';
+import React, { Suspense } from 'react';
+import {
+  Link,
+  NavLink,
+  Outlet,
+  defer,
+  Await,
+  useLoaderData,
+} from 'react-router-dom';
+
+// Data import
+import { getHostVans } from '../../api';
+
+export function loader({ params }) {
+  return defer({ van: getHostVans(params.id) });
+}
 
 function HostVanDetail() {
-  const { id } = useParams();
-  const [currentVan, setCurrentVan] = useState(null);
+  const loaderData = useLoaderData();
 
   const activeStyles = {
     color: '#161616',
@@ -14,62 +27,56 @@ function HostVanDetail() {
     opacity: 1,
   };
 
-  useEffect(() => {
-    fetch(`/api/host/vans/${id}`)
-      .then((res) => res.json())
-      .then((data) => setCurrentVan(data.vans));
-  }, []);
-
-  if (!currentVan) {
-    return (
-      <div className="min-h-screen pt-12 font-bold text-lg">Loading...</div>
-    );
-  }
-
   return (
     <section className="p-4 grid">
       <Link to=".." relative="path" className="back-button flex justify-start">
         &larr; <span>Back to all vans</span>
       </Link>
 
-      <div className="host-van-detail-layout-container">
-        <div className="host-van-detail flex">
-          <img src={currentVan.imageUrl} className="w-1/2" />
-          <div className="host-van-detail-info-text">
-            <i className={`van-type van-type-${currentVan.type}`}>
-              {currentVan.type}
-            </i>
-            <h3>{currentVan.name}</h3>
-            <h4>{currentVan.price}€/day</h4>
-          </div>
-        </div>
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <Await resolve={loaderData.van}>
+          {(currentVan) => (
+            <div className="host-van-detail-layout-container">
+              <div className="host-van-detail flex">
+                <img src={currentVan.imageUrl} className="w-1/2" />
+                <div className="host-van-detail-info-text">
+                  <i className={`van-type van-type-${currentVan.type}`}>
+                    {currentVan.type}
+                  </i>
+                  <h3>{currentVan.name}</h3>
+                  <h4>{currentVan.price}€/day</h4>
+                </div>
+              </div>
 
-        <nav className="mt-6 mb-4 grid gap-2 items-center justify-start text-sm opacity-80 xxs:flex xxs:gap-4 sm:gap-10 sm:max-w-xl sm:mx-auto">
-          <NavLink
-            to="."
-            end
-            className="py-1 px-2 rounded-md font-bold opacity-70 xs:py-2 xs:px-4"
-            style={({ isActive }) => (isActive ? activeStyles : null)}
-          >
-            Details
-          </NavLink>
-          <NavLink
-            to="pricing"
-            className="py-1 px-2 rounded-md font-bold opacity-70 xs:py-2 xs:px-4"
-            style={({ isActive }) => (isActive ? activeStyles : null)}
-          >
-            Pricing
-          </NavLink>
-          <NavLink
-            to="photos"
-            className="py-1 px-2 rounded-md font-bold opacity-70 xs:py-2 xs:px-4"
-            style={({ isActive }) => (isActive ? activeStyles : null)}
-          >
-            Photos
-          </NavLink>
-        </nav>
-        <Outlet context={{ currentVan }} />
-      </div>
+              <nav className="mt-6 mb-4 grid gap-2 items-center justify-start text-sm opacity-80 xxs:flex xxs:gap-4 sm:gap-10 sm:max-w-xl sm:mx-auto">
+                <NavLink
+                  to="."
+                  end
+                  className="py-1 px-2 rounded-md font-bold opacity-70 xs:py-2 xs:px-4"
+                  style={({ isActive }) => (isActive ? activeStyles : null)}
+                >
+                  Details
+                </NavLink>
+                <NavLink
+                  to="pricing"
+                  className="py-1 px-2 rounded-md font-bold opacity-70 xs:py-2 xs:px-4"
+                  style={({ isActive }) => (isActive ? activeStyles : null)}
+                >
+                  Pricing
+                </NavLink>
+                <NavLink
+                  to="photos"
+                  className="py-1 px-2 rounded-md font-bold opacity-70 xs:py-2 xs:px-4"
+                  style={({ isActive }) => (isActive ? activeStyles : null)}
+                >
+                  Photos
+                </NavLink>
+              </nav>
+              <Outlet context={{ currentVan }} />
+            </div>
+          )}
+        </Await>
+      </Suspense>
     </section>
   );
 }
