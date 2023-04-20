47b771c4f7da67574d3220831ae11e7246b8ccd7
Patrick Lemmer <patrick.lemmer@yahoo.com>
2023-04-20 16:50:50 +0200

    fixed styling on /vansdetails

--- src/pages/Vans/VanDetail.jsx
+++ src/pages/Vans/VanDetail.jsx
@@ -29,16 +29,18 @@ function VanDetail() {
       <Link
         to={`../..${search}`}
         relative="path"
-        className="back-button flex justify-start"
+        className="back-button flex justify-start font-bold ml-4 mb-4 sm:ml-12 sm:mb-12"
       >
-        &larr; <span>Back to {type} vans</span>
+        &larr; <span className="ml-2">Back to {type} vans</span>
       </Link>
       <Suspense fallback={<h2>Loading van...</h2>}>
         <Await resolve={loaderData.van}>
           {(van) => (
-            <div className="py-6 px-3 flex flex-col sm:max-w-2xl sm:mx-auto">
+            <div className="py-6 px-3 flex flex-col max-w-3xl mx-auto sm:px-6 lg:px-8">
               <img src={van.imageUrl} className="rounded-md mb-6" />
-              <i className={`van-type ${van.type} selected mb-6 max-w-[100px]`}>
+              <i
+                className={`van-type ${van.type} selected mb-6 max-w-[100px] sm:max-w-[200px]`}
+              >
                 {van.type}
               </i>
               <h2 className="text-xl font-bold text-left mb-4">{van.name}</h2>
@@ -58,3 +60,35 @@ function VanDetail() {
 }
 
 export default VanDetail;
+
+{
+  /* <div className="van-detail-container">
+<Link
+  to={`../..${search}`}
+  relative="path"
+  className="back-button flex justify-start"
+>
+  &larr; <span>Back to {type} vans</span>
+</Link>
+<Suspense fallback={<h2>Loading van...</h2>}>
+  <Await resolve={loaderData.van}>
+    {(van) => (
+      <div className="py-6 px-3 flex flex-col sm:max-w-2xl sm:mx-auto">
+        <img src={van.imageUrl} className="rounded-md mb-6" />
+        <i className={`van-type ${van.type} selected mb-6 max-w-[100px]`}>
+          {van.type}
+        </i>
+        <h2 className="text-xl font-bold text-left mb-4">{van.name}</h2>
+        <p className="van-price text-left mb-3">
+          <span className="font-bold">{van.price}€</span>/day
+        </p>
+        <p className="text-justify leading-6 mb-6">{van.description}</p>
+        <button className="link-button max-w-1/2 bg-orange-500 text-white text-[0.9rem] py-2 px-4 rounded-md font-bold text-center xxs:max-w-[280px] xxs:text-md xs:text-base sm:text-lg">
+          Rent this van
+        </button>
+      </div>
+    )}
+  </Await>
+</Suspense>
+</div> */
+}
