3d3f5bfe3119e22c858a0499b617fe1a7d9b9612
Patrick Lemmer <patrick.lemmer@yahoo.com>
2023-04-20 19:06:36 +0200

    fixed styling on host/vans/pricing

--- src/pages/Host/HostVanPricing.jsx
+++ src/pages/Host/HostVanPricing.jsx
@@ -6,9 +6,9 @@ import { useOutletContext } from 'react-router-dom';
 function HostVanPricing() {
   const { currentVan } = useOutletContext();
   return (
-    <h3>
-      {currentVan.price}€<span>/day</span>
-    </h3>
+    <h4 className="van-price text-left mb-3">
+      <span className="font-bold">{currentVan.price}€</span>/day
+    </h4>
   );
 }
 
